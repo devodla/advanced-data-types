a87b9de5bb1fed6695e9a91f0a78edfcd3c08e47
Huillca Reynaldo <lymreynaldo@icloud.com>
2022-05-22 23:28:34 -0300

    feat: add coercion type of objects

--- symbol.js
+++ symbol.js
@@ -38,3 +38,35 @@ const obj = {
 // }
 
 assert.deepStrictEqual([...obj], ["a", "b", "c"]);
+
+const kItems = Symbol("kItems");
+class MyDate {
+  constructor(...args) {
+    this[kItems] = args.map((arg) => new Date(...arg));
+  }
+
+  [Symbol.toPrimitive](coercionType) {
+    if (coercionType !== "string") throw new TypeError();
+
+    const items = this[kItems].map((item) =>
+      new Intl.DateTimeFormat("pt-BR", { month: "long", day: "2-digit", year: "numeric" }).format(item)
+    );
+
+    return new Intl.ListFormat("pt-BR", { style: "long", type: "conjunction" }).format(items);
+  }
+
+  get [Symbol.toStringTag]() {
+    return "WHAT?";
+  }
+}
+
+const myDate = new MyDate([2020, 03, 01], [2018, 02, 02]);
+
+const expectedDates = [new Date(2020, 03, 01), new Date(2018, 02, 02)];
+
+assert.deepStrictEqual(Object.prototype.toString.call(myDate), "[object WHAT?]");
+assert.throws(() => myDate + 1, TypeError);
+
+// coercao explicita para chamar o toPrimitive
+assert.deepStrictEqual(String(myDate), "01 de abril de 2020 e 02 de março de 2018");
+// console.log(String(myDate));
